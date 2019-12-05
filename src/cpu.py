bab85146f1262c250fe8eb815d8088ffe8c51f28
iibrahimli <imranibrahimli98@gmail.com>
2019-12-05 17:48:21 +0400

    add functionality to cpu

--- src/cpu.py
+++ src/cpu.py
@@ -1,7 +1,52 @@
-class simulator:
+"""
+Central Processing Unit
+
+"""
+
+from util import *
+from message import *
+
+
+class cpu:
     """
     Central processing unit for the station interlocking system
 
+    Attributes:
+        switchpoints (dict) : Element name (str) -> state (int)
+        signals (dict)      : Element name (str) -> state (int)
+
     """
 
-    pass
+    def __init__(self):
+        self.elements = {
+            0: {},  # signals
+            1: {}   # switchpoints
+        }
+    
+
+    def get_elem_state(self, e_type, elem):
+        """
+        Get the state of a given element
+
+        Arguments:
+            e_type (int)  : Element type
+            elem (str)    : Element name
+
+        Returns:
+            e_state (int) : Element state
+
+        """
+
+        return self.elements[e_type].get(elem, None)
+
+
+    def recv_message(self, m: message):
+        """
+        Receive and parse message
+
+        Arguments:
+            m (message) : The message
+
+        """
+
+        self.elements[m.e_type][m.elem] = m.e_state
