06eabae6c145793eb6fd849b1dd4983750c4a47f
Xaliq <xaliq2299@mail.ru>
2019-12-27 12:16:02 +0400

    added signals and routes after resetting + removed split part everywhere

--- src/cpu.py
+++ src/cpu.py
@@ -24,11 +24,29 @@ class cpu:
         """
         
         self._reset()
+       
+        # switchpoints 
         self.elements[SWITCHPOINT]['2a']=""
         self.elements[SWITCHPOINT]['1a']=""
         self.elements[SWITCHPOINT]['1']=""
         self.elements[SWITCHPOINT]['2']=""
         self.current_route = ""
+
+        # signals
+        self.elements[SIGNAL]['A1_3']=""
+        self.elements[SIGNAL]['A2_3']=""
+        self.elements[SIGNAL]['A2_4']=""
+        self.elements[SIGNAL]['E1_z2']=""
+        self.elements[SIGNAL]['E1_z1']=""
+        self.elements[SIGNAL]['E2_z2']=""
+
+        #routes
+        self.elements[ROUTE]['A1 A3']=""
+        self.elements[ROUTE]['A2 A3']=""
+        self.elements[ROUTE]['A2 A4']=""
+        self.elements[ROUTE]['E1 z2']=""
+        self.elements[ROUTE]['E1 z1']=""
+        self.elements[ROUTE]['E2 z2']=""
 
     
     def _reset(self):
@@ -110,51 +128,39 @@ class cpu:
         # checking signal safety
         if message(e_type, elem, e_state) == message(SIGNAL, 'A1_3', GREEN)  or \
            message(e_type, elem, e_state) == message(SIGNAL, 'A2_3', GREEN)  or \
-       message(e_type, elem, e_state) == message(SIGNAL, 'A2_4', GREEN)  or \
+           message(e_type, elem, e_state) == message(SIGNAL, 'A2_4', GREEN)  or \
            message(e_type, elem, e_state) == message(SIGNAL, 'E1_z2', GREEN) or \
            message(e_type, elem, e_state) == message(SIGNAL, 'E1_z1', GREEN) or \
-         message(e_type, elem, e_state) == message(SIGNAL, 'E2_z2', GREEN):
-          return True
+           message(e_type, elem, e_state) == message(SIGNAL, 'E2_z2', GREEN):
+            return True
 
         # checking route safety
         elif message(e_type, elem, e_state) == message(ROUTE, 'A1 A3', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['1a'] == PLUS:
             return True
            else:
             return False
         elif message(e_type, elem, e_state) == message(ROUTE, 'A2 A3', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['2a'] == PLUS and self.elements[SWITCHPOINT]['1a'] == MINUS:
             return True
            else:
             return False
         elif message(e_type, elem, e_state) == message(ROUTE, 'A2 A4', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['2a'] == PLUS:
             return True
            else:
             return False
         elif message(e_type, elem, e_state) == message(ROUTE, 'E1 z2', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['1'] == PLUS and self.elements[SWITCHPOINT]['1a'] == MINUS and self.elements[SWITCHPOINT]['2a'] == MINUS:
             return True
            else:
             return False
         elif message(e_type, elem, e_state) == message(ROUTE, 'E1 z1', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['1'] == PLUS and self.elements[SWITCHPOINT]['1a'] == PLUS:
             return True
            else:
             return False
         elif message(e_type, elem, e_state) == message(ROUTE, 'E2 z2', SET):
-           l=elem.split(' ')
-           print(l)
            if self.elements[SWITCHPOINT]['2a'] == PLUS:
             return True
            else:
